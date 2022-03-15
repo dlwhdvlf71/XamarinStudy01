5719a482b2f38f7b8f32bef15cd4066ff7d2d87b
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-03-15 15:58:32 +0900

    Commit
    Style cs 파일내에서 생성 및 Style 상속 로직

--- XamarinStudy01/StyleEx.xaml.cs
+++ XamarinStudy01/StyleEx.xaml.cs
@@ -15,6 +15,63 @@ namespace XamarinStudy01
         public StyleEx()
         {
             InitializeComponent();
+
+            var csMainTitleStyle = new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter
+                    {
+                        Property = Label.TextColorProperty,
+                        Value = "#fff",
+                    },
+                    new Setter 
+                    {
+                        Property = BackgroundColorProperty,
+                        Value = Color.Red
+                    },
+                    new Setter
+                    {
+                        Property = PaddingProperty,
+                        Value = 5
+                    }
+                    
+                }
+            };
+
+            var csSubTitleStyle = new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter
+                    {
+                        Property = Label.TextColorProperty,
+                        Value= "#fff",
+                    },
+                    new Setter
+                    {
+                        Property= BackgroundColorProperty,
+                        Value = Color.FromHex("#900")
+                    },
+                    new Setter
+                    {
+                        Property = PaddingProperty,
+                        Value = new Thickness(5)
+                    }
+                }
+            };
+
+            Label label01 = new Label();
+            label01.Text = "cs Label 01";
+            label01.Style = csMainTitleStyle;
+
+            Label label02 = new Label();
+            label02.Text = "cs Label 02";
+            label02.Style = csSubTitleStyle;
+
+            mainStackLayout.Children.Add(label01);
+            mainStackLayout.Children.Add(label02);
+
         }
     }
 }
