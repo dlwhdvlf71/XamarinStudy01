05e58bf8aa8a2d37644d0e427bb4c614c716db3e
dlwhdvlf71 <dlwhdvlf71@gmail.com>
2022-04-27 23:46:33 +0900

    Commit
    생명주기 Test

--- XamarinStudy01/App.xaml.cs
+++ XamarinStudy01/App.xaml.cs
@@ -15,16 +15,28 @@ namespace XamarinStudy01
 
         }
 
+        /// <summary>
+        /// 애플리케이션이 시작될 때 호출 된다.
+        /// </summary>
         protected override void OnStart()
         {
+            Console.WriteLine("OnStart");
         }
 
+        /// <summary>
+        /// 애플리케이션이 백그라운드로 이동할 때 호출된다.
+        /// </summary>
         protected override void OnSleep()
         {
+            Console.WriteLine("OnSleep");
         }
 
+        /// <summary>
+        /// 애플리케이션이 백그라운드에서 다시 시작할 때 호출된다.
+        /// </summary>
         protected override void OnResume()
         {
+            Console.WriteLine("OnResume");
         }
     }
 }
