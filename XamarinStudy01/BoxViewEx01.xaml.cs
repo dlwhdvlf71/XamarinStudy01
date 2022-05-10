29facb4e624869d02e44d715444a5dc0fe7cb460
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-05-10 23:52:38 +0900

    Commit
    2022-05-10

--- XamarinStudy01/BoxViewEx01.xaml.cs
+++ XamarinStudy01/BoxViewEx01.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,23 @@ namespace XamarinStudy01
         public BoxViewEx01()
         {
             InitializeComponent();
+           
+            //Console.WriteLine($"width : {Application.Current.MainPage.Height} / height : {Application.Current.MainPage.Height}");
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+
+
+            Console.WriteLine($"width : {mainDisplayInfo.Width} / height : {mainDisplayInfo.Height}");
+
+            BoxView boxView = new BoxView()
+            {
+                Color = Color.Black,
+                CornerRadius = 10,
+                WidthRequest = 30,
+                HeightRequest = 30
+            };
+
+            boxStackLayout.Children.Add(boxView);
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
---
+++ XamarinStudy01/Class/Box.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinStudy01.Class
+{
+    public class Box
+    {
+
+
+    }
+}
